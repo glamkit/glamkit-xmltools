4ec2f2a0c65662fa334cfcfe58becc2aaefcc0b4
Greg Turner <assembla@gregturner.com>
2011-01-05 15:38:12 +1100

    getfiles() can cope with being passed a file as well as a folder.

--- xmltools/lib/getfiles.py
+++ xmltools/lib/getfiles.py
@@ -6,15 +6,20 @@ def getfiles(path, regex=r"", recursive=True, followlinks=True):
     
     rex = re.compile(regex)
 
-    if recursive:    
-        for root, dirs, files in os.walk(path, followlinks):
-            for f in files:
-                path = os.path.abspath(os.path.join(root, f))
-                if rex.search(path):
-                    yield path
-    else:
-        for f in os.listdir(path):
-            p = os.path.abspath(os.path.join(path, f))
-            if os.path.isfile(p):
-                if rex.search(p):
-                    yield p
+    if os.path.isfile(path):
+        path = os.path.abspath(path)
+            if rex.search(path):
+                yield path
+    else: 
+        if recursive:    
+            for root, dirs, files in os.walk(path, followlinks):
+                for f in files:
+                    p = os.path.abspath(os.path.join(root, f))
+                    if rex.search(p):
+                        yield p
+        else:
+            for f in os.listdir(path):
+                p = os.path.abspath(os.path.join(path, f))
+                if os.path.isfile(p):
+                    if rex.search(p):
+                        yield p
